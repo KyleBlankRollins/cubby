23c27e5c8d56f73ee27b78325f4b72b852d30d31
Kyle Rollins <kyleblankrollins@gmail.com>
2023-06-01 21:45:46 -0500

    Add icons to bottom tabs

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Cubby
+
+## Icons
+
+- `react-native-vector-icons` repo: https://github.com/oblador/react-native-vector-icons
+- `react-native-vector-icons` directory: https://oblador.github.io/react-native-vector-icons/
--- app/navigation/Tabs.tsx
+++ app/navigation/Tabs.tsx
@@ -1,13 +1,21 @@
 import * as React from 'react';
+import {useColorScheme} from 'react-native';
+
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import {BottomTabNavigatorParamList} from './types';
 import HomeStackNavigator from './HomeStack';
+import {light, dark} from '../styles/theme';
+
 import {FindBookScreen} from '../screens/FindBookScreen';
 
 const Tab = createBottomTabNavigator<BottomTabNavigatorParamList>();
 
 const BottomTabs = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+  const themeColors = isDarkMode ? dark : light;
+
   return (
     <Tab.Navigator backBehavior="order">
       {/* TODO: reconcile stack navigator with tab navigator
@@ -15,11 +23,30 @@ const BottomTabs = () => {
       <Tab.Screen
         name="Home"
         component={HomeStackNavigator}
-        options={{headerShown: false}}
+        options={{
+          headerShown: false,
+          tabBarActiveTintColor: themeColors.main,
+          tabBarLabelPosition: 'beside-icon',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="bookshelf" color={color} size={size} />
+          ),
+        }}
       />
-      <Tab.Screen name="Find a book" component={FindBookScreen} />
+      <Tab.Screen
+        name="Find a book"
+        component={FindBookScreen}
+        options={{
+          tabBarActiveTintColor: themeColors.main,
+          tabBarLabelPosition: 'beside-icon',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="book-search" color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
+
+// const BookShelfIcon = <Icon name="mdiBookshelf" size={30} color="#900" />;
 
 export default BottomTabs;
